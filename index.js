62ac117d75062bfaaceb518b68818915a2dfc771
Joel Magnér <joel.magner@bonniernews.se>
2023-03-01 12:47:56 +0100

    1.0.4

--- index.js
+++ index.js
@@ -9,13 +9,4 @@ globalThis.Then = test;
 globalThis.And = test;
 globalThis.But = test;
 
-const Feature = globalThis.Feature;
-const Scenario = globalThis.Scenario;
-const BeforeEachScenario = globalThis.BeforeEachScenario;
-const Given = globalThis.Given;
-const When = globalThis.When;
-const Then = globalThis.Then;
-const And = globalThis.And;
-const But = globalThis.But;
-
 export { Feature, Scenario, Given, When, Then, And, But, BeforeEachScenario };
